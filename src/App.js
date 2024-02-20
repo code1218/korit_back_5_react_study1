e81209da75b7632f45ac5f4a2572bc52df7d5aa6
code1218 <skjil1218@gmail.com>
2024-02-20 17:38:09 +0900

    feat: useState, useEffect, useRef

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import StudentInfo from './components/StudentInfo';
+import InfoInput from './components/InfoInput';
+import InfoButtons from './components/InfoButtons';
 
 function App() {
   const studentObj = {
@@ -11,32 +12,20 @@ function App() {
 
   const [ student, setStudent ] = useState(studentObj);
   const [ inputValues, setInputValues ] = useState(studentObj);
-  const [ refresh, setRefresh ] = useState(false);
 
-  useEffect(() => {
-    if(refresh){
-      setInputValues(studentObj);
-    }
-    setRefresh(true);
-  }, [student]);
-
-  let email = "email";
-  let phone = "01099881916";
-
-  let user = {
-    username: "test",
-    "password": "1234",
-    [email]: "test",
-    phone
+  const inputRef = {
+    name: useRef(),
+    age: useRef(),
+    address: useRef()
   }
 
-  /**
-   * js객체 특징
-   * 1. 키값은 문자열이어도 된다.
-   * 2. 변수의 문자열 값을 키값으로 쓰고 싶을 때 []대괄호로 묶어서 참조할 수 있다.
-   * 3. 변수명만 입력하면 객체의 속성과 value로 한번에 정의할 수 있다.
-  */ 
+  useEffect(() => {
+    console.log(inputRef.name.current);
+  }, [])
 
+  useEffect(() => {
+    setInputValues(studentObj);
+  }, [student]);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -57,30 +46,38 @@ function App() {
 
   return (
     <>
-      <h1>이름: {student.name}</h1>
-      <h1>나이: {student.age}</h1>
-      <h1>주소: {student.address}</h1>
+      <StudentInfo title="이름" text={student.name} />
+      <StudentInfo title="나이" text={student.age} />
+      <StudentInfo title="주소" text={student.address} />
 
-      <input type="text" 
-        name='name' 
-        onChange={handleInputChange} 
+      <InfoInput 
+        name={"name"} 
+        onChange={handleInputChange}
         value={inputValues.name} 
-        placeholder='이름'/>
+        placeholder='이름'
+        inputRef={inputRef.name}
+      />
 
-      <input type="text" 
-        name='age' 
-        onChange={handleInputChange} 
+      <InfoInput 
+        name={"age"} 
+        onChange={handleInputChange}
         value={inputValues.age} 
-        placeholder='나이'/>
+        placeholder='나이'
+        inputRef={inputRef.age}
+      />
 
-      <input type="text" 
-        name='address' 
-        onChange={handleInputChange} 
+      <InfoInput 
+        name={"address"} 
+        onChange={handleInputChange}
         value={inputValues.address} 
-        placeholder='주소'/>
+        placeholder='주소'
+        inputRef={inputRef.address}
+      />
 
-      <button onClick={handleOnOk}>확인</button>
-      <button onClick={handleOnClean}>비우기</button>
+      <InfoButtons>
+        <button onClick={handleOnOk}>확인</button>
+        <button onClick={handleOnClean}>비우기</button>
+      </InfoButtons>
     </>
   );
 }
